a7c3915bfd45abe01abf036ec1b883c622f8d2d2
zentek-mikemichel <mike.michel@zentek.de>
2025-02-13 10:23:18 +0100

    feat: Create questionaire command and handler

---
+++ MultipleChoiceTool.Service/Commands/CreateQuestionaireCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using MultipleChoiceTool.Core.Commands;
+using MultipleChoiceTool.Core.Models;
+using MultipleChoiceTool.Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultipleChoiceTool.Service.Commands
+{
+    public class CreateQuestionaireCommandHandler : IRequestHandler<CreateQuestionaireCommand, QuestionaireModel>
+    {
+        private readonly IBaseWriteRepository<QuestionaireModel> _questionaireWriteRepository;
+
+        public CreateQuestionaireCommandHandler(
+            IBaseWriteRepository<QuestionaireModel> questionaireWriteRepository)
+        {
+            _questionaireWriteRepository = questionaireWriteRepository;
+        }
+
+        public Task<QuestionaireModel> Handle(CreateQuestionaireCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
