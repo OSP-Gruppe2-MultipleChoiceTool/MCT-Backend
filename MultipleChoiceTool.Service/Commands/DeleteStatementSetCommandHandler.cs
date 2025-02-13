eabc6dd2374887031ac0482754c5405db6ef5655
zentek-mikemichel <mike.michel@zentek.de>
2025-02-13 14:10:46 +0100

    feat: Implement statement set endpoints

---
+++ MultipleChoiceTool.Service/Commands/DeleteStatementSetCommandHandler.cs
@@ -0,0 +1,45 @@
+﻿using MediatR;
+using MultipleChoiceTool.Core.Commands;
+using MultipleChoiceTool.Core.Models;
+using MultipleChoiceTool.Core.Queries;
+using MultipleChoiceTool.Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultipleChoiceTool.Service.Commands
+{
+    internal class DeleteStatementSetCommandHandler : IRequestHandler<DeleteStatementSetCommand, StatementSetModel?>
+    {
+        private readonly IBaseWriteRepository<QuestionaireModel> _questionaireWriteRepository;
+        private readonly IMediator _mediator;
+
+        public DeleteStatementSetCommandHandler(
+            IBaseWriteRepository<QuestionaireModel> questionaireWriteRepository,
+            IMediator mediator)
+        {
+            _questionaireWriteRepository = questionaireWriteRepository;
+            _mediator = mediator;
+        }
+
+        public async Task<QuestionaireModel?> Handle(DeleteStatementSetCommand request, CancellationToken cancellationToken)
+        {
+            var questionaire = await _mediator.Send(new GetQuestionaireByIdQuery(request.QuestionaireId));
+            if (questionaire == null)
+            {
+                return null;
+            }
+
+            var statementSet = questionaire.StatementSets.FirstOrDefault(statementSet => statementSet.Id == request.statementSetId);
+            if (statementSet == null)
+            {
+                return null;
+            }
+
+            questionaire.StatementSets.Remove(statementSet);
+            return await _questionaireWriteRepository.UpdateAsync(questionaire, cancellationToken);
+        }
+    }
+}
---
+++ MultipleChoiceTool.Service/Queries/GetAllStatementSetsQueryHandler.cs
@@ -0,0 +1,37 @@
+﻿using MediatR;
+using MultipleChoiceTool.Core.Models;
+using MultipleChoiceTool.Core.Queries;
+using MultipleChoiceTool.Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultipleChoiceTool.Service.Queries
+{
+    internal class GetAllStatementSetsQueryHandler : IRequestHandler<GetAllStatementSetsQuery, IEnumerable<StatementSetModel>?>
+    {
+        private readonly IBaseReadRepository<QuestionaireModel> _questionaireReadRepository;
+        private readonly IMediator _mediator;
+
+        public GetAllStatementSetsQueryHandler(
+            IBaseReadRepository<QuestionaireModel> questionaireReadRepository,
+            IMediator mediator)
+        {
+            _questionaireReadRepository = questionaireReadRepository;
+            _mediator = mediator;
+        }
+
+        public async Task<IEnumerable<StatementSetModel>?> Handle(GetAllStatementSetsQuery request, CancellationToken cancellationToken)
+        {
+            var questionaireModel = await _questionaireReadRepository.FindByIdAsync(request.QuestionaireId, cancellationToken);
+            if (questionaireModel == null)
+            {
+                return null;
+            }
+
+            return questionaireModel.StatementSets;
+        }
+    }
+}
