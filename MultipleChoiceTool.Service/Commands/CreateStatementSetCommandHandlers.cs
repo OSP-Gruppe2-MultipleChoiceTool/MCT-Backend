2277a7437f2b220d9dd091f90ff5bd9fc6c763b8
zentek-mikemichel <mike.michel@zentek.de>
2025-02-13 11:41:52 +0100

    feat: Add statement set endpoint

---
+++ MultipleChoiceTool.Service/Commands/CreateStatementSetCommandHandlers.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using MultipleChoiceTool.Core.Commands;
+using MultipleChoiceTool.Core.Models;
+using MultipleChoiceTool.Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultipleChoiceTool.Service.Commands
+{
+    public class CreateStatementSetCommandHandlers : IRequestHandler<CreateStatementSetCommand, StatementSetModel>
+    {
+        private readonly IBaseWriteRepository<StatementSetModel> _statementSetWriteRepository;
+
+        public CreateStatementSetCommandHandlers(
+            IBaseWriteRepository<StatementSetModel> statementSetWriteRepository)
+        {
+            _statementSetWriteRepository = statementSetWriteRepository;
+        }
+
+        public Task<StatementSetModel> Handle(CreateStatementSetCommand request, CancellationToken cancellationToken)
+        {
+            return _statementSetWriteRepository.CreateAsync(request.StatementSet, cancellationToken);
+        }
+    }
+}
