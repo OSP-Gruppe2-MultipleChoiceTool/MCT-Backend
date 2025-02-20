4f6381eb81c4f3f5a17f1b1c9c8f32a0f15436ca
zentek-mikemichel <mike.michel@zentek.de>
2025-02-20 10:11:23 +0100

    feat: Implement user access

---
+++ MultipleChoiceTool.Core/Queries/GetQuestionaireByLinkIdQuery.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+using MultipleChoiceTool.Core.Models;
+
+
+namespace MultipleChoiceTool.Core.Queries;
+
+public record GetQuestionaireByLinkIdQuery(Guid LinkId, bool IsExam) : IRequest<QuestionaireModel?>;
+
