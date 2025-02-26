18181cf482cd21054158d25cd052b10ec0dee629
Paolo V <paolo.viola@cgi.com>
2025-02-26 19:58:07 +0100

    Added cors settings

--- MultipleChoiceTool.API/Extensions/ServiceExtensions.cs
+++ MultipleChoiceTool.API/Extensions/ServiceExtensions.cs
@@ -1,11 +1,33 @@
 ﻿using MultipleChoiceTool.API.Mappings;
+using MultipleChoiceTool.API.Settings;
 
 namespace MultipleChoiceTool.API.Extensions;
 
 public static class ServiceExtensions
 {
-    public static void AddMultipleChoiceToolApi(this IServiceCollection services)
+    public static void AddMultipleChoiceToolApi(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddAutoMapper(typeof(ApiMappings));
+        services.AddMultipleChoiceToolCors(configuration);
+    }
+
+    private static void AddMultipleChoiceToolCors(this IServiceCollection services, IConfiguration configuration)
+    {
+        var corsSettings = configuration.GetSection("Cors")?.Get<CorsSettings>();
+
+        services.AddCors(options =>
+        {
+            options.AddPolicy(CorsSettings.PolicyName, builder =>
+            {
+                builder.WithOrigins(corsSettings?.AllowedOrigins ?? ["*"])
+                       .WithMethods(corsSettings?.AllowedMethods ?? ["*"])
+                       .WithHeaders(corsSettings?.AllowedHeaders ?? ["*"]);
+                
+                if (corsSettings?.AllowCredentials == true)
+                {
+                    builder.AllowCredentials();
+                }
+            });
+        });
     }
 }
